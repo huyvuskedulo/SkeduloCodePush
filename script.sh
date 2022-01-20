3df7b320986df05cf7e2ebdb9a81ae50c5624a8d
Chi Ma <cma@skedulo.com>
2022-01-20 19:06:52 +1000

    Upload script

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 yarn react-native bundle --entry-file index.js --platform ios --dev false --bundle-output ios/main.jsbundle --assets-dest ios
 
 cd ios
+rm main.zip
 zip main.zip ./main.jsbundle ./assets
 
 curl \
